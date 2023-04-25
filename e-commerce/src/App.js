e08669345c9c3edb6a1628dfe7301f5d5b6de689
NitinAkuch <110688610+NitinAkuch@users.noreply.github.com>
2023-04-25 21:11:43 +0530

    created Login, Product, ProductList, Register Pages.

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-import Home from "./Pages/Home";
+import ProductList from "./Pages/ProductList";
+import Register from "./Pages/Register";
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <Register />
     </div>
   );
 };
