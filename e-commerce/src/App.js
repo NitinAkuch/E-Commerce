e833e66af860b33b2d53759eba218583921c39ba
NitinAkuch <110688610+NitinAkuch@users.noreply.github.com>
2023-12-20 00:04:57 +0530

    Component Success

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -12,6 +12,7 @@ import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
+import Success from "./Pages/Success";
 
 const App = () => {
   const user = false;
@@ -29,6 +30,9 @@ const App = () => {
         </Route>
         <Route path="/register">
           {user ? <Redirect to="/" /> : <Register />}
+        </Route>
+        <Route path="/success">
+          <Success />
         </Route>
         <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
         <Route path="/product/:id">
