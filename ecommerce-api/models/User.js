995270f929e6a286f33c7689c1d4c25016657214
NitinAkuch <110688610+NitinAkuch@users.noreply.github.com>
2023-12-20 00:27:46 +0530

    img added in User

--- ecommerce-api/models/User.js
+++ ecommerce-api/models/User.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-    {
-        userName: { type: String, required: true, unique: true},
-        email: { type: String, required: true, unique: true},
-        password: { type: String, required: true },
-        isAdmin: {type: Boolean, default: false}
-    },
-    { timestamps:true }
+  {
+    userName: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+    img: { type: String },
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model("User", userSchema);
